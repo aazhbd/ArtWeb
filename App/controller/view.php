9b4991636e455b3495ea31e817e0dc57743c0de2
AAZ H <aazhbd@yahoo.com>
2015-11-18 17:35:16 +0100

    Fix

--- App/controller/view.php
+++ App/controller/view.php
@@ -12,7 +12,7 @@ class Views extends Controller
                 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
             )
         );
-        $this->display($app, 'base.twig');
+        $this->display($app, 'home.twig');
     }
 
     public function viewCustom($params, $app) {
