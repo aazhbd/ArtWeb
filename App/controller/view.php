c1a3b91d81f3664ffc075735d2eb5307fa9ef487
aazhbd <aazhbd@yahoo.com>
2015-12-02 10:17:52 +0000

    New template added with new css for forms, menus and banner with new banner structures.

--- App/controller/view.php
+++ App/controller/view.php
@@ -8,7 +8,7 @@ class Views extends Controller
     {
         $app->setTemplateData(
             array(
-                'title' => 'Test',
+                'title' => 'Test home page',
                 'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
             )
         );
@@ -18,7 +18,7 @@ class Views extends Controller
     public function viewCustom($params, $app) {
         $app->setTemplateData(
             array(
-                'title' => 'This is the custom page',
+                'title' => 'This is custom page',
                 'body_content' => 'A test custom page loaded from controller/view.php.'
             )
         );
